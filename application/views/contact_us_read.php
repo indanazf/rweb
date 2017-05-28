4431faae07e7f91855fabbae963e4ddc6e7313e1
Anshar Abdullah <abdullah.anshar@gmail.com>
2017-05-28 14:02:34 +0700

    Contact us, tambah tombol reply. reply menggunakan default mail default setiap pc (misalnya outlook).

--- application/views/contact_us_read.php
+++ application/views/contact_us_read.php
@@ -1,20 +1,25 @@
 
-        <!-- Main content -->
-        <section class='content'>
-          <div class='row'>
-            <div class='col-xs-12'>
-              <div class='box'>
-                <div class='box-header'>
-                <h3 class='box-title'>Contact_us Read</h3>
-        <table class="table table-bordered">
-	    <tr><td>NAME CONTACT</td><td><?php echo $NAME_CONTACT; ?></td></tr>
-	    <tr><td>NO CONTACT</td><td><?php echo $NO_CONTACT; ?></td></tr>
-	    <tr><td>EMAIL</td><td><?php echo $EMAIL; ?></td></tr>
-	    <tr><td>TEXT</td><td><?php echo $TEXT; ?></td></tr>
-	    <tr><td></td><td><a href="<?php echo site_url('contact_us') ?>" class="btn btn-default">Back</a></td></tr>
-	</table>
-        </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!-- /.col -->
-          </div><!-- /.row -->
+<!-- Main content -->
+<section class='content'>
+  <div class='row'>
+    <div class='col-xs-12'>
+      <div class='box'>
+        <div class='box-header'>
+          <h3 class='box-title'>Contact_us Read</h3>
+          <table class="table table-bordered">
+           <tr><td>NAME CONTACT</td><td><?php echo $NAME_CONTACT; ?></td></tr>
+           <tr><td>NO CONTACT</td><td><?php echo $NO_CONTACT; ?></td></tr>
+           <tr><td>EMAIL</td><td><?php echo $EMAIL; ?></td></tr>
+           <tr><td>TEXT</td><td><?php echo $TEXT; ?></td></tr>
+           <tr><td></td>
+            <td>
+              <a href="<?php echo site_url('contact_us') ?>" class="btn btn-default">Back</a>
+              <a class="btn btn-success" title="reply" href="mailto:<?= $EMAIL?>"="_top"><i class="fa fa-reply"></i> Reply</a>
+            </td>
+          </tr> 
+        </table>
+      </div><!-- /.box-body -->
+    </div><!-- /.box -->
+  </div><!-- /.col -->
+</div><!-- /.row -->
         </section><!-- /.content -->
