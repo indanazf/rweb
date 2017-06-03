b28a5930ce9ccf13b99abc3418da7b41edec639e
Candra <candradwiprasetyo@gmail.com>
2017-06-04 00:32:28 +0700

    add section 4

---
+++ application/views/layouts/about_us/section4.php
@@ -0,0 +1,38 @@
+<div class="banner-4">
+	<div class="row">
+		<div class="col-md-6">
+			<div class="banner-4__content">
+				<div class="banner-4__content__title">
+					GET INVOLVED
+				</div>
+				<div class="banner-4__content__desc">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					<div class="button__readmore"></div>
+				</div>
+			</div>
+		</div>
+			
+		<div class="col-md-6">
+			<div class="banner-4__join-us">
+				<div class="banner-4__join-us__circle">Join Us</div>
+			</div>
+			<div>
+				<div class="banner-4__contact-us">
+					<div class="banner-4__contact-us__icon">
+					</div>
+					<div class="banner-4__contact-us__title">
+						Contact Us
+					</div>
+				</div>
+				<div class="banner-4__faq">
+					<div class="banner-4__faq__icon">
+					</div>
+					<div class="banner-4__faq__title">
+						FAQ
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+   
