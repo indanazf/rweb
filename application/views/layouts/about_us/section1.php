0eb56acfbb4bcb257add32f24fd5959336d9d1a2
indanazf <izulfa28@gmail.com>
2017-06-10 12:04:25 +0700

    tes carousell

--- application/views/layouts/about_us/section1.php
+++ application/views/layouts/about_us/section1.php
@@ -1,5 +1,12 @@
+<div id="myCarousel" class="banner carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+    <li data-target="#myCarousel" data-slide-to="4"></li>
+  </ol>
 <?php foreach($about_us as $row){?>
-<div class="banner">
 	<div class="banner-overlay">
 		<img src="<?= base_url('uploads/'.$row->IMG) ?>">
 	</div>
@@ -10,5 +17,5 @@
 			<a href="<?php echo site_url($row->LINK) ?>"><div class="button__readmore__long"><?=$row->TAGS?></div></a>
 		</div>
 	</div>
-</div>
 <?php } ?>
+</div>
