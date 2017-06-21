74dd32367ea9a3d01ef3f7dd3b0bc30d42b2074d
Candra <candradwiprasetyo@gmail.com>
2017-06-21 10:29:20 +0700

    fix

--- assets/js/function.js
+++ assets/js/function.js
@@ -35,11 +35,16 @@ $(function() {
 });
 
 function banner2_readmore(){
-  $(".banner-2__content").css({'float': 'right', 'right': '0'});
-  $(".banner-2__content").stop().animate({width:'150%'}, 500);
+  var media = window.innerWidth;
+  if(media >= 1000){
+    $(".banner-2__content").css({'float': 'right', 'right': '0'});
+    $(".banner-2__content").stop().animate({width:'150%'}, 500);
+    $(".banner-2__content__desc").stop().animate({'height': '200px'}, 500);
+  }else{
+    $(".banner-2__content__desc").stop().animate({'height': '240px'}, 500);
+  }
   $("#banner-2__content__readmore").hide();
   $("#banner-2__content__close").show();
-  $(".banner-2__content__desc").stop().animate({'height': '200px'}, 500);
 }
 
 function banner2_close(){
